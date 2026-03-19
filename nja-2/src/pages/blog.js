e2eb80e1c24369a0ea047dc1e91fb1248fde2e7a
DeathCode <dc.deathcode@gmail.com>
2026-03-19 12:54:34 +0530

    Page Router vs App Router

---
+++ nja-2/src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Blogs() {
+  return (
+    <div>
+        <h1>List of blogs</h1>
+        <p>Blog 1</p>
+        <p>Blog 2</p>
+        <p>Blog 3</p>
+        <p>Blog 4</p>
+
+    </div>
+  )
+}
